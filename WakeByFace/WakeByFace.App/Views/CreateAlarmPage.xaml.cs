1634cf46f8f4d18870f48ad421b7f53fc977eff0
StasVerenchuk <stasverenchuk@gmail.com>
2025-12-02 12:05:07 +0200

    Update page layout for adding new alarm

--- WakeByFace/WakeByFace.App/Views/CreateAlarmPage.xaml.cs
+++ WakeByFace/WakeByFace.App/Views/CreateAlarmPage.xaml.cs
@@ -2,8 +2,38 @@ namespace WakeByFace.App.Views;
 
 public partial class CreateAlarmPage : ContentPage
 {
+
 	public CreateAlarmPage()
 	{
 		InitializeComponent();
 	}
+
+    private void OnDayTapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void OnDayChecked(object sender, CheckedChangedEventArgs e)
+    {
+        var cb = (CheckBox)sender;
+
+        if (e.Value) // увімкнено
+        {
+            cb.BackgroundColor = (Color)Resources["OrangeColor"];
+        }
+        else // вимкнено
+        {
+            cb.BackgroundColor = (Color)Resources["SectionsColor"];
+        }
+    }
+
+    private void ConfirmButtonClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void DismissButtonClicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
 }
